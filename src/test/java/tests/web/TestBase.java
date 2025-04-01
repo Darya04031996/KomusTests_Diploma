bf6bd1169892cd5f614eda1957245264ec3a3c38
Darya Melgunova <darya.melgunova@gmail.com>
2025-04-01 21:00:37 +0300

    added25

--- src/test/java/tests/web/TestBase.java
+++ src/test/java/tests/web/TestBase.java
@@ -3,8 +3,11 @@ package tests.web;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import common.config.AuthConfig;
+import common.config.WebConfig;
 import common.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +15,14 @@ import org.junit.jupiter.api.BeforeEach;
 public class TestBase {
     @BeforeAll
     static void setupConfig() {
-        //WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
-        //AuthConfig authConfig = ConfigFactory.create(AuthConfig.class, System.getProperties());
+        WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
+        AuthConfig authConfig = ConfigFactory.create(AuthConfig.class, System.getProperties());
+        Configuration.baseUrl = webConfig.baseUrl();
+        Configuration.browser = webConfig.browser();
+        Configuration.browserVersion = webConfig.browserVersion();
+        Configuration.browserSize = webConfig.browserSize();
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
 
         Configuration.browserSize = "920x1080";
         Configuration.pageLoadStrategy = "eager";
